81b5910c9b86fedf0318d146169d9314cfd3da78
l1fad <lexa77159@gmail.com>
2021-05-17 21:34:26 +0700

    start header

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCKTYPE html>
+<html lang = "ru">
+<head>
+	<meta charset = "UTF-8">
+	<meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content = "ie=edge">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+	<title>Document</title>
+</head>
+<body>
+	<header class="p-3 bg-dark text-white">
+    <div class="container">
+
+      <div class="d-flex flex-wrap align-items-center justify-content-start justify-content-end">
+      <h4 class="my-0 mr-md-auto font-weight-normal"><a href = "/autosell" class = "nav-link px-4 text-white">AutoSell</a></h4>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Вход</button>
+          <button type="button" class="btn btn-info">Регистрация</button>
+        </div>
+      </div>
+    </div>
+  </header>
+</body>
+</html>
